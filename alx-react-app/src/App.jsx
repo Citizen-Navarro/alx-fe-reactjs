578e38b9470f02f19dee767968a0fa3903bde49d
Citizen Navarro <navarro.citizen@gmail.com>
2025-07-09 17:48:36 +0200

    feat: add Header, MainContent, Footer components and integrate into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,33 +1,45 @@
-return (
-  <>
-    <Header />
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-    </div>
+function App() {
+  const [count, setCount] = useState(0)
 
-    <h1>Vite + React</h1>
+  return (
+    <>
+      <Header />
 
-    <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <h1>Vite + React</h1>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
       </p>
-    </div>
 
-    <p className="read-the-docs">
-      Click on the Vite and React logos to learn more
-    </p>
-
-    <WelcomeMessage />
-    <MainContent />
-    <Footer />
-  </>
-);
+      <WelcomeMessage />
+      <MainContent />
+      <Footer />
+    </>
+  );
