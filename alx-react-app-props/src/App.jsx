e4f4bab94dd6e82fabee505e3be7d87d72deb338
Citizen Navarro <navarro.citizen@gmail.com>
2025-07-16 01:33:28 +0200

    feat: refactor prop drilling using Context API in alx-react-app-props

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,52 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import ProfilePage from './components/ProfilePage';
+import UserContext from './context/UserContext';
+
+function App() {
+  const userData = { name: "Alice", age: "25", bio: "Loves hiking and photography" };
+
+  return (
+    <>
+      <UserContext.Provider value={userData}>
+        <Header />
+
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+
+        <h1>Vite + React</h1>
+
+        <div className="card">
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+
+        <WelcomeMessage />
+        <MainContent />
+
+        {/* 👇 Refactored User Profile Flow using Context */}
+        <ProfilePage />
+
+        <Footer />
+      </UserContext.Provider>
+    </>
+  );
+}
+
+export default App;
