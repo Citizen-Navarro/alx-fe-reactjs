934e827c277dff561dc2efd4bb0a860de23f80b8
Citizen Navarro <navarro.citizen@gmail.com>
2025-07-24 01:59:27 +0200

    Fix: Add Router wrapper to App.jsx for routing check

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,29 +1,31 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Recipe Sharing App</h1>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <hr />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <Router>
+      <div style={{ padding: '2rem' }}>
+        <h1>Recipe Sharing App</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <hr />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
