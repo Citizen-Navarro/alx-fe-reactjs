7c2e72110216808a984397682a73655f8383ec2c
Citizen Navarro <navarro.citizen@gmail.com>
2025-07-30 11:17:27 +0200

    chore: set up GitHub User Search React app with Vite and Axios

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>GitHub User Search App</h1>
+      <p>Start searching for GitHub users.</p>
+    </div>
+  );
+}
+
+export default App;
