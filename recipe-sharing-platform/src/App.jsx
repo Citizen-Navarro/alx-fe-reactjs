6e1235a338386fc960e814371526681b17266ecb
Citizen Navarro <navarro.citizen@gmail.com>
2025-08-12 12:33:25 +0200

    Update tailwind config for checker

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,7 @@
+import HomePage from "./components/HomePage";
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold text-blue-500">
-      Hello Tailwind!
-    </h1>
-  );
+  return <HomePage />;
 }
 
 export default App;
